6bd5beb23faf8545dad68b7f7153db83d143294b
Alone107 <123736524+Alone107@users.noreply.github.com>
2023-05-01 22:58:44 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,22 +3,23 @@ $('.reviews__slider').slick({
   centerPadding: '0px',
   slidesToShow: 1,
   slidesToScroll: 3,
+  adaptiveHeight: true,
   responsive: [
     {
       breakpoint: 768,
       settings: {
-        arrows: false,
         centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
+        centerPadding: '0px',
+        adaptiveHeight: true,
+        slidesToShow: 1
       }
     },
     {
       breakpoint: 480,
       settings: {
-        arrows: false,
         centerMode: true,
-        centerPadding: '40px',
+        centerPadding: '0px',
+        adaptiveHeight: true,
         slidesToShow: 1
       }
     }
@@ -26,4 +27,31 @@ $('.reviews__slider').slick({
 });
 Fancybox.bind("[data-fancybox]", {
   // Your custom options
+});
+
+$('.chef__btn-link').on('click', function (e) {
+  e.preventDefault()
+  $('.chef__popup').toggleClass('chef__popup--open')
+  $('body').toggleClass('scroll')
+  $('.overlay').toggleClass('overlay--show')
+});
+$('.chef__popup-btn').on('click', function (e) {
+  e.preventDefault()
+  $('.chef__popup-2').toggleClass('chef__popup-2--open')
+  $('.chef__popup').removeClass('chef__popup--open')
+  $('body').toggleClass('scroll')
+});
+$('.footer__inner-forma-button, .overlay').on('click', function (e) {
+  e.preventDefault()
+  $('.chef__popup-3').toggleClass('chef__popup-3--open')
+  $('body').toggleClass('scroll')
+  $('.overlay').toggleClass('overlay--show')
+});
+$('.overlay').on('click', function (e) {
+  e.preventDefault()
+  $('.chef__popup-2').removeClass('chef__popup-2--open')
+  $('.chef__popup-3').removeClass('chef__popup-3--open')
+  $('.chef__popup').removeClass('chef__popup--open')
+  $('body').removeClass('scroll')
+  $('.overlay').removeClass('overlay--show')
 });
